a03d6c20a7170e248fa83979489d093170e1eae4
Nikolaos Dimopoulos <nikos@niden.net>
2016-05-03 01:46:44 -0400

    Minor changes to the image

--- mariadb-alpine/support/entrypoint.sh
+++ mariadb-alpine/support/entrypoint.sh
@@ -20,9 +20,7 @@ if [ ! -f /data/db/mysql/ibdata1 ]; then
     /usr/bin/mysqld_safe --defaults-file=/data/conf/my.cnf &
     sleep 10s
 
-    echo "UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASS}') WHERE User='root'; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DELETE FROM mysql.user WHERE User=''; DROP DATABASE test; FLUSH PRIVILEGES;" | mysql -u root
-
-    echo "GRANT ALL ON *.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION; GRANT ALL ON *.* TO ${DB_USER}@'localhost' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION; GRANT ALL ON *.* TO ${DB_USER}@'::1' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql -u root --password="${DB_ROOT_PASS}"
+    echo "UPDATE mysql.user SET Host='%', Password=PASSWORD('${DB_ROOT_PASS}') WHERE User='root'; DELETE FROM mysql.user WHERE User=''; DROP DATABASE test; FLUSH PRIVILEGES;" | mysql -u root
 
     killall mysqld
     killall mysqld_safe
